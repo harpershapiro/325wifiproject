5387303ad67d65b5e838e5ad51debc153884bb5a
harper shapiro <harper.shapiro@gmail.com>
2019-11-03 17:26:16 -0600

    worked on receiver

--- wifi_bjay/wifi/Receiver.java
+++ wifi_bjay/wifi/Receiver.java
@@ -1,6 +1,19 @@
 package wifi;
 
 public class Receiver implements Runnable {
+    private int mac;
 
+    public Receiver(int mac){
+        this.mac = mac;
+    }
+
+    public byte[] getData(){
+        return null;
+    }
+
+    @Override
+    public void run(){
+        //receive
+    }
 
 }
--- wifi_bjay/wifi/Sender.java
+++ wifi_bjay/wifi/Sender.java
@@ -5,6 +5,12 @@ public class Sender implements Runnable {
     private boolean retry; //true if we are resending current packet
     private boolean fullyIdle; //true if we have never seen the channel busy during this transmission
 
+    public Sender(int mac){
+        this.mac = mac;
+        this.retry = false;
+        this.fullyIdle = false;
+    }
+
     @Override
     public void run(){
         //Wait for something to arrive on the outgoing data queue
@@ -12,7 +18,29 @@ public class Sender implements Runnable {
             //Make packet
             //Check if medium idle
             //not idle:
+
                 //wait until idle
+                //channel is idle:
+                    //wait DIFS
+                    //channel still idle:
+                        //wait exponential backoff time
+                        //channel interupted:
+                            //save exponential timer state
+                            //go back to wait DIFS step
+                        //exponential timer finished:
+                            //transmit frame
+                            //wait for an ack
+                            //ack received:
+                                //set fully idle to false
+                                //go back to beginning
+                            //ack not received in time:
+                                //up contention window
+                                //set retry to true
+                                //go back to beginning, new packet out of same data from before
+                    //channel not still idle:
+                        //back to waiting for channel to be idle
+
+
             //idle:
                 //fullyIdle = true;
                 //wait DIFS
@@ -23,6 +51,9 @@ public class Sender implements Runnable {
                         //set fully idle to false
                         //go back to beginning
                     //ack not received in time:
+                        //up contention window
+                        //set retry to true
+                        //go back to beginning, new packet out of same data from before
     }
 
 }
