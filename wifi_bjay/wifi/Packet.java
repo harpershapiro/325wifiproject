330f42cf5df21dcaebb4032190b8c6297f0fe387
SkylerLoftus <skylerloftus@gmail.com>
2019-12-09 17:24:15 -0800

    CRC logic edited

--- wifi_bjay/wifi/Packet.java
+++ wifi_bjay/wifi/Packet.java
@@ -172,11 +172,11 @@ public class Packet {
 
     public static int extractCRC(byte[] b) {
         int crc = 0;
-        crc = ((b[b.length-4] & 0xff) << 24);   //is the int value of crc from packet
-        crc = ((b[b.length-3] & 0xff) << 16);   //is the int value of crc from packet
-        crc = ((b[b.length-2] & 0xff) << 8);   //is the int value of crc from packet
+        crc += ((b[b.length-4] & 0xff) << 24);   //is the int value of crc from packet
+        crc += ((b[b.length-3] & 0xff) << 16);   //is the int value of crc from packet
+        crc += ((b[b.length-2] & 0xff) << 8);   //is the int value of crc from packet
 
-        crc = ((b[b.length-1] & 0xff) | crc);
+        crc += ((b[b.length-1] & 0xff));
         return crc;
     }
 
